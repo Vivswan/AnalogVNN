ebddf3fa622c4c653a21513b6bee70e7ce997eb5
Vivswan Shah <shahvivswan@gmail.com>
2022-10-17 03:04:57 -0400

    updated copyright

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,8 +3,8 @@
 # -- Project information
 
 project = 'AnalogVNN'
-copyright = '2021, Graziella'
-author = 'Graziella'
+copyright = '2020-2022, Vivswan Shah (vivswanshah@pitt.edu)'
+author = 'Vivswan Shah'
 
 release = '0.1'
 version = '0.1.0'
