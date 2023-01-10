67d4ab68b217a568ce05dcd9280195b214941873
Vivswan Shah <58091053+Vivswan@users.noreply.github.com>
2023-01-10 14:36:52 -0500

    updated docs dependencies
    Signed-off-by: Vivswan Shah <58091053+Vivswan@users.noreply.github.com>

--- research/sample_code_with_logs.py
+++ research/sample_code_with_logs.py
@@ -248,7 +248,7 @@ def run_linear3_model():
 
     # Training
     print(f"Starting Training...")
-    for epoch in range(10):
+    for epoch in range(1):
         train_loss, train_accuracy = nn_model.train_on(train_loader, epoch=epoch)
         test_loss, test_accuracy = nn_model.test_on(test_loader, epoch=epoch)
 
