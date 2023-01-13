1da1e7ba4f082539226aa52b0f36d36232107813
Tianyi Zheng <tianyizheng02@gmail.com>
2023-01-13 15:30:53 -0500

    super already calls forward_graph connection

--- analogvnn/nn/module/FullSequential.py
+++ analogvnn/nn/module/FullSequential.py
@@ -24,6 +24,5 @@ class FullSequential(Sequential):
             FullSequential: self
         """
         arr = [self.graphs.INPUT, *list(self._runtime_module_list.values()), self.graphs.OUTPUT]
-        self.graphs.forward_graph.add_connection(*arr)
         self.graphs.backward_graph.add_connection(*reversed(arr))
         return super().compile(device, layer_data)
