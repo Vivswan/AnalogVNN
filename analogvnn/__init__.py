0a603395283480a41865614bb1e2ed5528a24039
Vivswan Shah <58091053+Vivswan@users.noreply.github.com>
2023-01-13 22:50:53 -0500

    updated importlib_metadata

--- analogvnn/__init__.py
+++ analogvnn/__init__.py
@@ -1,14 +1,18 @@
 import sys
 
-import importlib_metadata
+if sys.version_info[:2] >= (3, 8):
+    from importlib import metadata
+else:
+    import importlib_metadata as metadata  # pragma: no cover
+
 
 __package__ = 'analogvnn'
 __author__ = 'Vivswan Shah (vivswanshah@pitt.edu)'
 
 try:
-    __version__ = importlib_metadata.version(__package__)
-except importlib_metadata.PackageNotFoundError:
-    __version__ = 'local'
+    __version__ = metadata.version(__package__)
+except metadata.PackageNotFoundError:
+    __version__ = '0.0.0'
 
 if sys.version_info < (3, 7, 0):
     import warnings
